d466065ee1cb3b807139b01816bbcfd5b0ca60ad
Alihan METE <110615796+alhnmtee@users.noreply.github.com>
2023-12-18 22:44:13 +0300

    test

--- Main.java
+++ Main.java
@@ -2,6 +2,12 @@
 public class Main {
     public static void main(String[] args) {
         Restaurant1 restaurant = new Restaurant1();
-        restaurant.start();
+        
+        
+
+
+
+
+
     }
 }
